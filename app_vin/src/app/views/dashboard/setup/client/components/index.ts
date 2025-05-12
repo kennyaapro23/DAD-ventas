89f2a1bfae5df9420e82477bd20a3173ab04fc04
kennyaapro23 <103528325+kennyaapro23@users.noreply.github.com>
2025-05-12 18:22:19 -0500

    new vv

--- app_vin/src/app/views/dashboard/setup/client/components/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './lists/roles-list.component';
--- app_vin/src/app/views/dashboard/setup/client/models/client.ts
+++
@@ -1,9 +0,0 @@
-export class Client {
-
-    id?: number;
-    nombre?: string;
-    apellidos?: string;
-    dni?: string;
-    telefono?: string;
-    correo?: string;
-}
