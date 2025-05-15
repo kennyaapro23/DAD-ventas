4b39aa6d66ad065fa60747d81743f512c7d8a0b5
kennyaapro23 <kennyaapro@hotmail.com>
2025-05-14 20:57:50 -0500

    Conflicto de fusión resuelto manualmente

---
+++ dad-ventas-frontend/.angular/cache/19.2.12/dad-ventas-frontend/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,42 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell
+} from "./chunk-FJ3ZJ7FP.js";
+import "./chunk-TPHTALPS.js";
+import "./chunk-ZDB7ZGYU.js";
+import "./chunk-I5SLBODZ.js";
+import "./chunk-MPLAZ3KA.js";
+import "./chunk-3BZSWGRX.js";
+import "./chunk-UO4WZBQ7.js";
+import "./chunk-NWDP3SJI.js";
+import "./chunk-YWNTH3TK.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  withAppShell,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
